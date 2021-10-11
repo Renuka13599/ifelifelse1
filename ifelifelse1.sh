2b5cb9fcc271521935d157faf27582ab3cd0ea28
renuka13599 <renuka13599@gmail.com>
2021-10-11 08:09:29 +0530

    ifelifelseprograms

---
+++ ifelifelse1.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -x
+echo "Enter single digit number"
+read number;
+if [ $number == 0 ]
+then
+echo "Zero"
+elif [ $number == 1 ]
+then
+echo "One"
+elif [ $number == 2 ]
+then
+echo "Two"
+elif [ $number == 3 ]
+then
+echo "Three"
+elif [$number == 4 ]
+then
+echo "Four"
+elif [ $number == 5 ]
+then
+echo "Five"
+elif [$number == 6 ]
+then
+echo "Six"
+elif [$number == 7 ]
+then
+echo "Seven"
+elif [ $number == 8 ]
+then
+echo "Eight"
+elif [ $number == 9 ]
+then
+echo "Nine"
+else
+echo "Not a single digit"
+fi
